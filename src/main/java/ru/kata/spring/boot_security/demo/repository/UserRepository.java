b365a5b31859e85e8c98b2f6425fc711a1d06b98
FomichevAYu <mailforspoify@yandex.ru>
2023-06-04 15:23:32 +0300

    add bootstrap

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import ru.kata.spring.boot_security.demo.models.User;
 
 public interface UserRepository extends JpaRepository <User, Long> {
-    //Такое решение позволяет не ставить транзакции на читающих методах и работают с "ленивой" загрузкой,
-    //что хорошо влияет на оптимизацию приложения
+
     @Query("Select u from User u left join fetch u.roles where u.username=:username")
     User findByUsername(String username);
     @Query("Select u from User u left join fetch u.roles where u.id=:id")
